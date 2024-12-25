dbb6e40798e682c053153249e7ab3f56adf395ee
REIGaruki <gyakujo@mail.ru>
2024-12-25 22:58:43 +0300

    Commit third step services

---
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -0,0 +1,23 @@
+package ru.skypro.homework.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.Ads;
+import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.dto.CreateOrUpdateAd;
+import ru.skypro.homework.dto.ExtendedAd;
+import ru.skypro.homework.model.Ad;
+
+import java.io.IOException;
+
+public interface AdService {
+
+    Ad addAd(CreateOrUpdateAd ad, MultipartFile image);
+    Ads getAllAds();
+    boolean exists(int id);
+    Comments getComments(int id);
+    ExtendedAd getExtendedAd(int id);
+    void deleteAd(int id);
+    Ad updateAd(int id, CreateOrUpdateAd adUpdate);
+    Ads getAdsByUser();
+    byte[] changeImage(int id, MultipartFile image) throws IOException;
+}
---
+++ src/main/java/ru/skypro/homework/service/impl/AdServiceImpl.java
@@ -0,0 +1,61 @@
+package ru.skypro.homework.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.Ads;
+import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.dto.CreateOrUpdateAd;
+import ru.skypro.homework.dto.ExtendedAd;
+import ru.skypro.homework.model.Ad;
+import ru.skypro.homework.service.AdService;
+
+import java.io.IOException;
+
+@Service
+public class AdServiceImpl implements AdService {
+
+    @Override
+    public Ad addAd(CreateOrUpdateAd ad, MultipartFile image) {
+        return null;
+    }
+
+    @Override
+    public Ads getAllAds() {
+        return null;
+    }
+
+    @Override
+    public boolean exists(int id) {
+        return false;
+    }
+
+    @Override
+    public Comments getComments(int id) {
+        return null;
+    }
+
+    @Override
+    public ExtendedAd getExtendedAd(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteAd(int id) {
+
+    }
+
+    @Override
+    public Ad updateAd(int id, CreateOrUpdateAd adUpdate) {
+        return null;
+    }
+
+    @Override
+    public Ads getAdsByUser() {
+        return null;
+    }
+
+    @Override
+    public byte[] changeImage(int id, MultipartFile image) throws IOException {
+        return image.getBytes();
+    }
+}
